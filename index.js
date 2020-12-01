508c0911a0f46dc0e25d9c46af1d5f690a791f35
Lakshya Khera <lakshya.khera@gmail.com>
2020-12-02 01:02:05 +0530

    Added CSRF Token

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
+var csrf = require("csurf");
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json("application/json"));
-
+app.use(cookieParser());
+app.use(csrf({ cookie: true }));
+app.use(csrf());
 app.get("/", (req, res) => res.send("Hello"));
 
 app.post("/profile", (req, res) => {
