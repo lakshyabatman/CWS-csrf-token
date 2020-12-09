6a2c7e3a765b5bcdc86a8e2d4633ed151657a5e7
Lakshya Khera <lakshya.khera@gmail.com>
2020-12-09 23:45:31 +0530

    Test

--- index.js
+++ index.js
@@ -4,11 +4,34 @@ const cookieParser = require("cookie-parser");
 const app = express();
 var csrf = require("csurf");
 app.use(bodyParser.urlencoded({ extended: true }));
+var cons = require("consolidate");
+
 // app.use(bodyParser.json("application/json"));
+var csrfProtection = csrf({ cookie: true });
 app.use(cookieParser());
-app.use(csrf({ cookie: true }));
-app.use(csrf());
-app.get("/", (req, res) => res.send("Hello"));
+
+// app.use(csrf());
+app.engine("hbs", cons.handlebars);
+
+// it defaults to 'jade'
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+app.use(function (err, req, res, next) {
+  if (err.code !== "EBADCSRFTOKEN") return next(err);
+
+  // handle CSRF token errors here
+  res.status(403);
+  res.send(
+    "<h1> CSFR Token was not present, please contact security team!</h1>"
+  );
+});
+
+app.get("/", csrfProtection, (req, res) =>
+  res.render("index", {
+    csrfToken: req.csrfToken(),
+  })
+);
 
 app.post("/profile", (req, res) => {
   console.log(req.body);
